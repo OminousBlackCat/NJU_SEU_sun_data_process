82d4c277b1edad30cda6009024a9c13dc1f58115
asdfzlcl <1105374599@qq.com>
2022-02-21 11:26:20 +0800

    更新红蓝翼矫正部分 等待具体数据调试

--- suntools.py
+++ suntools.py
@@ -1,4 +1,14 @@
 import numpy as np
+import re
+import os
+import time
+from astropy.io import fits
+import suntools
+import matplotlib.pyplot as plt
+from PIL import Image
+from astropy.utils.data import get_pkg_data_filename
+from astropy.io import fits
+import sys
 
 
 # 谱线矫正
@@ -56,11 +66,54 @@ def linefit(x, y):  # np.polyfit(X, Y, 1)
     b = (sy - k * sx) / N
     return k, b
 
+# 标准太阳光谱获取
+# 参数filepath：储存标准光谱的文件路径
+# 输出：文件包含数据
+def get_Sunstd(filepath):
+    print("标准光谱数据地址："+filepath)
+    data=[]
+    with open(filepath) as f :
+        line = f.readline()
+        if len(re.findall(r"\d+\.?\d*",line))>0:
+            data.append(float(re.findall(r"\d+\.?\d*", line)[0]))
+        while line :
+            line = f.readline()
+            if len(re.findall(r"\d+\.?\d*", line)) > 0:
+                data.append(float(re.findall(r"\d+\.?\d*", line)[0]))
+    return np.array(data)
+
 
 # 红蓝移矫正
-def RB_repair(data):
+def RB_repair(data,sun_std):
     H, W = data.shape
-    k, b = linefit(sum(data, axis=1), data)
+    k, b = linefit(np.arange(0, H, 1),np.sum(data, axis=1)/sun_std[0:H])
     for i in range(H):
         data[i, :] = data[i, :] / (k * i + b)
     return data
+
+
+if __name__ == "__main__":
+    print(get_Sunstd("teststd.txt"))
+    # print("suntools_test")
+    # # data = np.array([[0,1,2],[1,2,3]])
+    # # print(RB_repair(data,np.array([6,18])))
+    # filepath_result = "testResult\\"
+    # filepath_test = "testData\\"
+    # sys.path.append('../')
+    # sys.path.append(filepath_test)
+    # filelist = os.listdir(filepath_test)
+    # #print(filelist)
+    # data=np.zeros([376,4608])
+    # for i in range(400):
+    #     image_file = get_pkg_data_filename(filepath_test+filelist[2200+i])
+    #     image_data = fits.getdata(image_file)
+    #     data+=image_data
+    # data/=400
+    # data=Pingchang(data)
+    # data=Curve_correction(data, 2225, 0.01 / (2225 - 770) / (2225 - 770))
+    # image_file = get_pkg_data_filename(filepath_test + filelist[2288])
+    # image_data = fits.getdata(image_file)
+    # image_data=Curve_correction(image_data, 2225, 0.01 / (2225 - 770) / (2225 - 770))
+    # plt.figure()
+    # plt.imshow(image_data/data)
+    # plt.show()
