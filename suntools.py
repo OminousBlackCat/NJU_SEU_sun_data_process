425b1a2b016f4ad81324e80ed53ff415998a2dd6
asdfzlcl <1105374599@qq.com>
2022-02-23 10:50:00 +0800

    均值滤波代码修改效率大幅度提升

--- suntools.py
+++ suntools.py
@@ -40,9 +40,9 @@ def curve_correction(imgData, x0, C):
 # 输出: 平场数据
 def getFlat(flatData):
     H, W = flatData.shape
-    mean = np.sum(data, axis=1) / W
+    mean = np.sum(flatData, axis=1) / W
     for i in range(H):
-        flatData[i, :] = flatData[i, :] / flatData[i]
+        flatData[i, :] = flatData[i, :] / mean[i]
     return flatData
 
 
@@ -68,22 +68,34 @@ def linefit(x, y):  # np.polyfit(X, Y, 1)
 # 输出：文件包含数据
 def get_Sunstd(filepath):
     print("标准光谱数据地址：" + filepath)
-    data = []
+    dataX = []
+    dataY = []
     with open(filepath) as f:
         line = f.readline()
-        if len(re.findall(r"\d+\.?\d*", line)) > 0:
-            data.append(float(re.findall(r"\d+\.?\d*", line)[1]))
         while line:
+            line = re.findall(r"\d+\.?\d*", line)
+            if len(line) > 0:
+                dataX.append(float(line[0]))
+                dataY.append(float(line[1]))
             line = f.readline()
-            if len(re.findall(r"\d+\.?\d*", line)) > 0:
-                data.append(float(re.findall(r"\d+\.?\d*", line)[1]))
-    return np.array(data)
+    dataX = np.array(dataX)
+    dataX = (dataX - 6559.5804) / 0.024202301
+    now = 1
+    ansY = []
+    for i in range(400):
+        while(dataX[now]<i):
+            now+=1
+        ansY.append(dataY[now-1]+(dataY[now]-dataY[now-1])/(dataX[now]-dataX[now-1])*(i-dataX[now-1]))
+    ansY = np.array(ansY)
+    return ansY/np.max(ansY)
 
 
 # 红蓝移矫正
 def RB_repair(imgData, sun_std):
     H, W = imgData.shape
-    cov = np.polyfit(np.arange(0, H, 1), np.sum(imgData, axis=1) / W / sun_std[0:H], 1)
+    sun_image = np.sum(imgData, axis=1) / W
+    sun_image /= np.max(sun_image)
+    cov = np.polyfit(np.arange(0, H, 1), sun_image / sun_std[0:H], 1)
     k, b = cov[0], cov[1]
     # print(k,b)
     for i in range(H):
@@ -105,16 +117,22 @@ def subtract(data_A, data_B):
 
 # 图像平滑操作
 # 参数
-def smooth(imgData, winSize=5):
+def smooth(imgData, winSize=9):
     H, W = imgData.shape
     offset = int(winSize / 2)
+    OffsetData = np.zeros((H + offset * 2, W + offset * 2))
     SmoothData = np.zeros((H + offset * 2, W + offset * 2))
-    for i in range(offset, offset + H):
-        for j in range(offset, offset + W):
-            SmoothData[i][j] = int(imgData[i - offset][j - offset])
-    for i in range(offset, offset + H):
-        for j in range(offset, offset + W):
-            SmoothData[i][j] = np.sum(SmoothData[i - offset: i - offset + winSize, j - offset: j - offset + winSize]) / (winSize * winSize)
+    # for i in range(offset, offset + H):
+    #     for j in range(offset, offset + W):
+    #         SmoothData[i][j] = int(imgData[i - offset][j - offset])
+    OffsetData[offset:offset+H,offset:offset+W] = imgData[0:H,0:W]
+
+    # for i in range(offset, offset + H):
+    #     for j in range(offset, offset + W):
+    #         SmoothData[i][j] = np.sum(SmoothData[i - offset: i - offset + winSize, j - offset: j - offset + winSize]) / (winSize * winSize)
+    for i in range(winSize):
+        for j in range(winSize):
+            SmoothData[offset:offset+H,offset:offset+W] += OffsetData[i : H + i,j:W + j]
     # kernel = win*win-2
     # for x in range(H-win):
     #     for y in range(W-win):
@@ -124,21 +142,24 @@ def smooth(imgData, winSize=5):
 
 if __name__ == "__main__":
     filepath_result = "data/"
-    filepath_test = "data/"
-    filepath_bash = "data/bass2000.txt"
-    base = np.array(get_Sunstd(filepath_bash), dtype=float)
-    # print(filelist)
+    filepath_test = "testdata/"
+    filepath_bash = "bass2000.txt"
+    base = get_Sunstd(filepath_bash)
+    # print(base)
     image_file = get_pkg_data_filename(filepath_test + 'dark.fits')
     dark_data = np.array(fits.getdata(image_file), dtype=float)
 
     image_file = get_pkg_data_filename(filepath_test + 'for_flat.fits')
     flat_data = np.array(fits.getdata(image_file), dtype=float)
-
     data = curve_correction(flat_data, 2321.26, 1.92909e-011)
+    # data = smooth(data)
     data = getFlat(data)
     # plt.figure()
     # plt.imshow(data, cmap="gray")
     # plt.show()
+    plt.figure()
+    plt.imshow(data, cmap="gray")
+    plt.show()
     print("Ping is over")
     time_start = time.time()
     image_file = get_pkg_data_filename(filepath_test + "RSM20211222T060131-0008-2529.fts")
