803ba66a47923288be3daef1e96d64635aa57f8a
asdfzlcl <1105374599@qq.com>
2022-02-24 13:56:15 +0800

    包装完整内容

--- suntools.py
+++ suntools.py
@@ -52,8 +52,7 @@ def curve_correction(imgData, x0, C):
                 imgData[y+260][x] = stdy[now - 1] + (stdy[now] - stdy[now - 1]) / (stdx[now] - stdx[now - 1]) * (
                             y - stdx[now - 1])
     imgData[bad_H:bad_H+116] = imgData[260:376]
-    print(bad_H,bad_Fe)# 230 80
-    return imgData[0:bad_H+bad_Fe]
+    return imgData[0:bad_H+bad_Fe],bad_H,bad_Fe
 
 
 # 平场计算
@@ -97,15 +96,14 @@ def get_Sunstd(filepath):
 
 
 # 红蓝移矫正
-def RB_repair(imgData, sun_std):
+def RB_repair(imgData, sun_std, HofH, HofFe):
     H, W = imgData.shape
     sun_image = np.sum(imgData, axis=1) / W
     sun_image /= np.max(sun_image)
-    stdx = np.zeros(318)
-    sun_std[231:231+87] = sun_std[260:260+87]
-    stdx[0:231] = np.arange(0, 231, 1) * 0.024202301 + 6562.82
-    stdx[231:] = np.arange(0, 87, 1) * 0.024202301 + 6569.22
-    print(H)
+    stdx = np.zeros(H)
+    sun_std[HofH:HofH+HofFe] = sun_std[260:260+HofFe]
+    stdx[0:HofH] = np.arange(0, HofH, 1) * 0.024202301 + 6562.82
+    stdx[HofH:] = np.arange(0, HofFe, 1) * 0.024202301 + 6569.22
     cov = np.polyfit(stdx, sun_image / sun_std[0:H], 1)
     k, b = cov[0], cov[1]
     # print(k,b)
@@ -139,17 +137,10 @@ def MeanSmooth(imgData, winSize=4):
             SmoothData[offset:offset+H,offset:offset+W] += OffsetData[i : H + i,j:W + j]
     return SmoothData[offset: offset + H, offset: offset + W]/winSize/winSize
 
-def DivFlat(imgData,flatData):
+def DivFlat(imgData,flatData,HofH, HofFe):
     H, W = imgData.shape
-    # flatList = np.sum(flatData,axis=0)
-    # maxFlatindex = np.argmax(flatList)
-    # imgList = np.sum(imgData,axis=0)
-    # maxImgindex = np.argmax(imgList)
-    # offset = maxImgindex - maxFlatindex
-    # 230 80
-    # print(offset)
-    imgHa = imgData[0:230]
-    flatHa = flatData[0:230]
+    imgHa = imgData[0:HofH]
+    flatHa = flatData[0:HofH]
     flatList = np.sum(flatHa, axis=0)
     maxFlatindex = np.argmax(flatList)
     imgList = np.sum(imgHa, axis=0)
@@ -157,11 +148,11 @@ def DivFlat(imgData,flatData):
     offset = maxImgindex - maxFlatindex
     if offset < 0:
         offset *= -1
-        flatHa[0:230 - offset, 0:W - offset] = flatHa[offset:230,offset:W]
+        flatHa[0:HofH - offset, 0:W - offset] = flatHa[offset:HofH,offset:W]
     else:
-        flatHa[offset:230,offset:W] = flatHa[0:230 - offset, 0:W - offset]
-    imgFe = imgData[230:]
-    flatFe = flatData[230:]
+        flatHa[offset:HofH,offset:W] = flatHa[0:HofH - offset, 0:W - offset]
+    imgFe = imgData[HofH:]
+    flatFe = flatData[HofH:]
     flatList = np.sum(flatFe, axis=0)
     maxFlatindex = np.argmax(flatList)
     imgList = np.sum(imgFe, axis=0)
@@ -169,11 +160,11 @@ def DivFlat(imgData,flatData):
     offset = maxImgindex - maxFlatindex
     if offset < 0:
         offset *= -1
-        flatFe[0:80 - offset, 0:W - offset] = flatFe[offset:80, offset:W]
+        flatFe[0:HofFe - offset, 0:W - offset] = flatFe[offset:HofFe, offset:W]
     else:
-        flatFe[offset:80, offset:W] = flatFe[0:80 - offset, 0:W - offset]
-    flatData[0:230] = flatHa
-    flatData[230:] = flatFe
+        flatFe[offset:HofFe, offset:W] = flatFe[0:HofFe - offset, 0:W - offset]
+    flatData[0:HofH] = flatHa
+    flatData[HofH:] = flatFe
     return imgData/flatData
 
 # 图像中值平滑操作
@@ -187,6 +178,17 @@ def MedSmooth(imgData, winSize=4):
     #     for j in range(W-winSize):
     #         SmoothData[offset+i][offset+j]=np.median(imgData[i:i+winSize,j:j+winSize])
     return imgData
+
+def entireWork(filename,darkDate,flatData,sun_std):
+    image_file = get_pkg_data_filename(filename)
+    imgData = np.array(fits.getdata(image_file), dtype=float)
+    imgData,HofH,HofFe = curve_correction(imgData - darkDate, 2321.26, 1.92909e-011)
+    imgData = DivFlat(imgData, flatData, HofH, HofFe)
+    imgData = RB_repair(imgData, sun_std, HofH, HofFe)
+    imgData = MedSmooth(imgData)
+    return imgData
+
+
 
 
 if __name__ == "__main__":
@@ -214,7 +216,7 @@ if __name__ == "__main__":
     # plt.figure()
     # plt.imshow(flat_data, cmap="gray", aspect='auto')
     # plt.show()
-    data = curve_correction(flat_data , 2321.26, 1.92909e-011)
+    data,H,F = curve_correction(flat_data , 2321.26, 1.92909e-011)
     # plt.figure()
     # plt.imshow(data, cmap="gray",aspect='auto')
     # plt.show()
@@ -223,40 +225,15 @@ if __name__ == "__main__":
     # plt.imshow(data, cmap="gray",aspect='auto')
     # plt.show()
     print("Ping is over")
-    plt.figure()
     time_start = time.time()
-    image_file = get_pkg_data_filename(filepath_test + filelist[3000])
-    test_data = np.array(fits.getdata(image_file), dtype=float)
-    plt.subplot(5, 1, 1)
-    plt.imshow(test_data - dark_data, cmap="gray",aspect='auto')
-    #plt.title('去暗场')
-    test_data = curve_correction(test_data - dark_data, 2321.26, 1.92909e-011)
-    plt.subplot(5, 1, 2)
-    plt.imshow(test_data, cmap="gray",aspect='auto')
-    #plt.title('谱线矫正')
-    H, W = data.shape
-    # data[2:H,2:W] = data[0:H-2,0:W-2]
-    test_data = DivFlat(test_data,data)
-    plt.subplot(5, 1, 3)
-    plt.imshow(test_data, cmap="gray",aspect='auto')
-    #plt.title('去平场')
-    test_data = RB_repair(test_data, base)
-    plt.subplot(5, 1, 4)
-    test_data = np.array(test_data, dtype=np.int16)
-    plt.imshow(test_data, cmap="gray",aspect='auto')
-    #plt.title('红蓝翼矫正')
-    time_end1 = time.time()
-    test_data = MedSmooth(test_data)
-    plt.subplot(5, 1, 5)
-    plt.imshow(test_data, cmap="gray",aspect='auto')
-    #plt.title('滤波')
+    image_file = entireWork(filepath_test + filelist[3000],dark_data,data,base)
+
     time_end = time.time()
     print(time_end - time_start)
-    print(time_end - time_end1)
     plt.figure()
-    plt.imshow(test_data, cmap="gray",aspect='auto')
+    plt.imshow(image_file, cmap="gray",aspect='auto')
     plt.show()
 
-    grey = fits.PrimaryHDU(test_data)
-    greyHDU = fits.HDUList([grey])
-    greyHDU.writeto(filepath_result+'result.fits')
+    # grey = fits.PrimaryHDU(image_file)
+    # greyHDU = fits.HDUList([grey])
+    # greyHDU.writeto(filepath_result+'result.fits')
