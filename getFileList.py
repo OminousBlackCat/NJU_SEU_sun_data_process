5407d1cdf7de59438838e7afda4206fd6da092ae
asdfzlcl <1105374599@qq.com>
2022-04-22 13:58:12 +0800

    目标分析获取

--- getFileList.py
+++ getFileList.py
@@ -1,17 +1,17 @@
 import config
 import os
-
 if config.bin_count == 1:
     limit_size = config.sun_row_count_bin_1
 else:
     limit_size = config.sun_row_count_bin_2
-
-
 def getTarget(filePath):
     dbtype_list = os.listdir(filePath)
     target_list = []
+    out_list = []
     if len(dbtype_list) >= limit_size:
         target_list.append(filePath)
+        nums = filePath.split('/')
+        out_list .append( nums[-1].split('-')[0])
     err = 0
     for dbtype in dbtype_list:
         try:
@@ -24,10 +24,17 @@ def getTarget(filePath):
         else:
             if (len(filelist)) >= limit_size:
                 target_list.append(filePath + dbtype)
+                nums = dbtype.split('/')
+                out_list .append( nums[-1].split('-')[0])
 
     print(target_list)
-    return target_list
-
+    print(out_list)
+    return target_list,out_list
 
 if __name__ == "__main__":
-    getTarget('D:/NJU_SEU_sun_data_process/')
+    #getTarget('D:/NJU_SEU_sun_data_process/')
+    filePath = 'D:/NJU_SEU_sun_data_process/3-23'
+    nums = filePath.split('/')
+    out_list = nums[-1].split('-')[0]
+    print(out_list)
+
