c24e23592c745d1641ee5e3cc2eb08f04462ecd8
asdfzlcl <1105374599@qq.com>
2022-04-22 08:51:05 +0800

    获取可使用文件目录

---
+++ getFileList.py
@@ -0,0 +1,22 @@
+import config
+import os
+if config.bin_count == 1:
+    limit_size = config.sun_row_count_bin_1
+else:
+    limit_size = config.sun_row_count_bin_2
+def getTarget(filePath):
+    dbtype_list = os.listdir(filePath)
+    target_list = []
+    if len(dbtype_list) >= limit_size:
+        target_list.append(filePath)
+    for dbtype in dbtype_list:
+        if os.path.isdir(filePath+dbtype):
+            filelist = os.listdir(filePath + dbtype)
+            if(len(filelist)) >= limit_size:
+                target_list.append(filePath + dbtype)
+    print(target_list)
+    return target_list
+
+if __name__ == "__main__":
+    getTarget('D:/NJU_SEU_sun_data_process/')
+
