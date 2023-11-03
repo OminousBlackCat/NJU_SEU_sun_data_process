67c803f89aca7ab9c84e650410bfd3fc32f83440
asdfzlcl <1105374599@qq.com>
2023-11-03 11:21:52 +0800

    NJU code combine

--- createVideo.py
+++ createVideo.py
@@ -10,12 +10,20 @@
 
 import cv2
 import os
+import numpy as np
+import glob
 
 from PIL import Image
 
 import config
 import suntools
 import traceback
+
+from math import *
+from astropy.io import fits
+import matplotlib.pyplot as plt
+
+
 
 framePerSec = config.frame_pre_sec
 pic_count = config.write_to_video_count
@@ -75,6 +83,80 @@ def createVideo():
     #         fe_videoOut.write(fe_img)
     ha_videoOut.release()
     # fe_videoOut.release()
+
+def createVideoMp4(img_array,filename):
+    size = (0,0)
+    for indexf in range(len(img_array)):  # 这个循环是为了读取所有要用的图片文件
+        img1 = cv2.imread(fileDir + img_array[indexf], 1)
+        if img1 is None:
+            print(img_array[indexf] + " is error!")
+            continue
+        size = (img1.shape[1], img1.shape[0])
+        img_array.append(img1)
+
+    videowrite = cv2.VideoWriter(filename, -1, 6, size)
+
+    for i in range(len(img_array)):  # 把读取的图片文件写进去
+        videowrite.write(img_array[i])
+
+    videowrite.release()
+
+def createVideoNJU():
+    getPNGList()
+    createVideoMp4(ha_list,saveDir + 'RSM' + fileDate.strftime('%Y-%m-%d') + '_HA.mp4')
+    createVideoMp4(fe_list, saveDir + 'RSM' + fileDate.strftime('%Y-%m-%d') + '_FE.mp4')
+
+def monographNJU(hadata_path0,fedata_path0):
+    ang_res = 0.5218 * 2
+    tick_pixel = [1040 - 1000 / ang_res, 1040 - 500 / ang_res, 1040, 1040 + 500 / ang_res, 1040 + 1000 / ang_res]
+    tick_arcsec = [-1000, -500, 0, 500, 1000]
+
+    for rsm_path in hadata_path0:
+        hdu = fits.open(rsm_path)
+        hacore = hdu[1].data[68, :, :]
+        cx = int(hdu[1].header['CRPIX1'])
+        cy = int(hdu[1].header['CRPIX2'])
+        datetime = rsm_path.split('/')[-1][3:7] + '-' + rsm_path.split('/')[-1][7:9] + '-' + rsm_path.split('/')[-1][
+                                                                                             9:11] + \
+                   ' UT ' + rsm_path.split('/')[-1][12:14] + ':' + rsm_path.split('/')[-1][14:16] + ':' + \
+                   rsm_path.split('/')[-1][16:18]
+        plt.figure(figsize=(20, 20))
+        plt.imshow(hacore[cy - 1040:cy + 1040, cx - 1040:cx + 1040], origin='lower', vmin=0, vmax=4 * hacore.mean(),
+                   cmap='afmhot')
+        plt.text(1950, 50, datetime, horizontalalignment='right', verticalalignment='bottom', color='white', size=28)
+        plt.xticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
+        plt.yticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
+        plt.xlabel('Solar X (arcsec)', fontsize=30)
+        plt.ylabel('Solar Y (arcsec)', fontsize=30)
+        plt.savefig('/data/home/MikeRao/data/Time_array/ha' + rsm_path.split('/')[-1][3:18] + 'full_sun.png', dpi=100)
+        plt.close()
+        print('/data/home/MikeRao/data/Time_array/ha' + rsm_path.split('/')[-1][3:18] + 'full_sun.png')
+
+    ang_res = 0.5218 * 2
+    tick_pixel = [1040 - 1000 / ang_res, 1040 - 500 / ang_res, 1040, 1040 + 500 / ang_res, 1040 + 1000 / ang_res]
+    tick_arcsec = [-1000, -500, 0, 500, 1000]
+
+    for rsm_path in fedata_path0:
+        hdu = fits.open(rsm_path)
+        hacore = hdu[1].data[10, :, :]
+        cx = int(hdu[1].header['CRPIX1'])
+        cy = int(hdu[1].header['CRPIX2'])
+        datetime = rsm_path.split('/')[-1][3:7] + '-' + rsm_path.split('/')[-1][7:9] + '-' + rsm_path.split('/')[-1][
+                                                                                             9:11] + \
+                   ' UT ' + rsm_path.split('/')[-1][12:14] + ':' + rsm_path.split('/')[-1][14:16] + ':' + \
+                   rsm_path.split('/')[-1][16:18]
+        plt.figure(figsize=(20, 20))
+        plt.imshow(hacore[cy - 1040:cy + 1040, cx - 1040:cx + 1040], origin='lower', vmin=0, vmax=4 * hacore.mean(),
+                   cmap='afmhot')
+        plt.text(1950, 50, datetime, horizontalalignment='right', verticalalignment='bottom', color='white', size=28)
+        plt.xticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
+        plt.yticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
+        plt.xlabel('Solar X (arcsec)', fontsize=30)
+        plt.ylabel('Solar Y (arcsec)', fontsize=30)
+        plt.savefig('/data/home/MikeRao/data/Time_array/fe' + rsm_path.split('/')[-1][3:18] + 'full_sun.png', dpi=100)
+        plt.close()
+        print('/data/home/MikeRao/data/Time_array/fe' + rsm_path.split('/')[-1][3:18] + 'full_sun.png')
+
 
 
 def main():
