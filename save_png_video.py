262a102e6d8fcda890d0df27fe0af8cc24915927
OminousBlackCat <wxy1999814@gmail.com>
2023-12-15 15:55:57 +0800

    bugfix: 去除生成预览像的额外白边

--- save_png_video.py
+++ save_png_video.py
@@ -56,7 +56,7 @@ def monographNJU(target_path: str, color_map, image_dpi = 100):
         plt.yticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
         plt.xlabel('Solar X (arcsec)', fontsize=30)
         plt.ylabel('Solar Y (arcsec)', fontsize=30)
-        plt.savefig(target_path + current_filename.split('.')[0]  + ".png", dpi=image_dpi)
+        plt.savefig(target_path + current_filename.split('.')[0]  + ".png", dpi=image_dpi, pad_inches=0.0, bbox_inches='tight')
         plt.close()
         # print('/data/home/MikeRao/data/Time_array/ha' + rsm_path.split('/')[-1][3:18] + 'full_sun.png')
 
@@ -80,7 +80,7 @@ def monographNJU(target_path: str, color_map, image_dpi = 100):
         plt.yticks(ticks=tick_pixel, labels=tick_arcsec, fontsize=22)
         plt.xlabel('Solar X (arcsec)', fontsize=30)
         plt.ylabel('Solar Y (arcsec)', fontsize=30)
-        plt.savefig(target_path + current_filename.split('.')[0] + ".png", dpi=image_dpi)
+        plt.savefig(target_path + current_filename.split('.')[0] + ".png", dpi=image_dpi, pad_inches=0.0, bbox_inches='tight')
         plt.close()
         # print('/data/home/MikeRao/data/Time_array/fe' + rsm_path.split('/')[-1][3:18] + 'full_sun.png')
 
@@ -138,7 +138,6 @@ def createVideoNJU(target_dir: str, save_dir: str):
         current_date_str = target_dir.split('/')[-3] + '-' + target_dir.split('/')[-2] + '-' + target_dir.split('/')[-1]
     createVideoMp4(target_dir, ha_list, save_dir + 'RSM' + current_date_str + '_HA.avi')
     createVideoMp4(target_dir, fe_list, save_dir + 'RSM' + current_date_str + '_FE.avi')
-
 
 if __name__ == '__main__':
     png_target_dir = video_config.path_to_target_png
--- swing_scan_multiprocess_up.py
+++ swing_scan_multiprocess_up.py
@@ -1053,9 +1053,10 @@ def main():
     # pool1.shutdown(wait=True)
     time_end = time.time()
     suntools.log('并行进度已完成，所花费时间为：', (time_end - time_start) / 60, 'min(分钟)')
-    suntools.log('生成预览图像与视频中...')
+    suntools.log('生成预览图像中...')
+    suntools.log('视频请使用脚本生成...')
     save_png_video.monographNJU(SUM_DIR, color_map, image_dpi=config.png_dpi_value)
-    save_png_video.createVideoNJU(SUM_DIR, config.video_dir_path)
+    # save_png_video.createVideoNJU(SUM_DIR, config.video_dir_path)
     suntools.log('程序结束！')
 
 if __name__ == "__main__":
